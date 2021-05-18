fb754edcb9dbdc0fbdd1f459336db693de501e12
Jasper Snel <me@jaspersnel.me>
2021-05-18 11:14:20 +0200

    Add support for custom server IP

--- demosetup.py
+++ demosetup.py
@@ -1,7 +1,14 @@
 from pathlib import Path
 from uuid import uuid4
+import argparse
 
 import yaml
+
+
+parser = argparse.ArgumentParser(description='Set up a demo Vantage6 infrastructure.')
+parser.add_argument('--server-ip', '-s', type=str, help='what IP will the server live (in case of non-local?', default='host.docker.internal')
+
+args = parser.parse_args()
 
 orgs = [{'database': db} for db in Path('databases').glob('*.csv') if db.is_file()]
 
@@ -71,10 +78,11 @@ with open(Path('skeletons/server-config-skeleton.yaml')) as f:
 with open(server_dir / 'config.yaml', 'w') as f:
     yaml.safe_dump(server_config, f)
 
+# Add server calls to run.sh
 script = script + f'vserver start --user -c {(server_dir / "config.yaml").resolve()}\n'
 script = script + f'vserver import --user --drop-all -c {(server_dir / "config.yaml").resolve()} {(server_dir / "entities.yaml").resolve()}\n'
 
-
+# Write the node configs
 with open(Path('skeletons/node-config-skeleton.yaml')) as f:
     node_skeleton = yaml.safe_load(f)
 
@@ -82,10 +90,9 @@ for org in orgs:
     node = node_skeleton.copy()
 
     node['application']['databases']['default'] = str(org['database'].resolve())
-
     node['application']['api_key'] = org['api_key']
 
-    # Enable encryption
+    node['application']['server_url'] = f'http://{args.server_ip}'
     node['application']['encryption']['enabled'] = False
     node['application']['encryption']['private_key'] = ''
 
@@ -93,6 +100,7 @@ for org in orgs:
     with open(output_file, 'w+') as f:
         yaml.safe_dump(node, f)
 
+    # Add run command for node to run.sh
     script = script + f'vnode start -c {output_file.resolve()}\n'
 
 with open('run.sh', 'w') as f:
